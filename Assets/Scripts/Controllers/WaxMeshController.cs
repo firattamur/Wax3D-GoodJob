a76a43c1ee7d4cc5d4fbb0c4a422ab8d8b4d6f3f
Fırat Tamur <ftamur16@ku.edu.tr>
2022-05-09 01:46:44 +0300

    feat: add remove wax mesh animation

--- Assets/Scripts/Controllers/WaxMeshController.cs
+++ Assets/Scripts/Controllers/WaxMeshController.cs
@@ -1,43 +1,100 @@
 using System;
 using GameState;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-public class WaxMeshController : MonoBehaviour
+namespace Controllers
 {
-
-    [SerializeField] private MeshRenderer meshRenderer;
-    
-    private void OnEnable()
+    public class WaxMeshController : MonoBehaviour
     {
 
-        WaxStickController.OnWaxStickAnimationStopped += DryWaxMesh;
-
-    }
-
-    private void OnDisable()
-    {
-        WaxStickController.OnWaxStickAnimationStopped -= DryWaxMesh;
-    }
-
-    void Start()
-    {
-         
-    }
-
-    void Update()
-    {
+        [SerializeField] private MeshRenderer meshRenderer;
+        [SerializeField] private MegaBend megaBend;
+        [SerializeField] private GameObject hairs;
+        [SerializeField] private float megaBendUpdateAngle = 2f;
+        [SerializeField] private float megaBendMaxAngle = 60f;
+            
+        private float _megaBendAngleIncrease;
         
-    }
+        private void OnEnable()
+        {
 
-    private void DryWaxMesh()
-    {
-        LeanTween.alpha(meshRenderer.gameObject, 1.0f, 2f).setOnComplete(OnWaxMeshDry);
-    }
+            WaxStickController.OnWaxStickAnimationStopped += DryWaxMesh;
 
-    private void OnWaxMeshDry()
-    {
-        GameManager.instance.SetState(new WaxRemoveState(GameManager.instance));
-    }
+        }
 
+        private void OnDisable()
+        {
+            WaxStickController.OnWaxStickAnimationStopped -= DryWaxMesh;
+        }
+
+        private void Update()
+        {
+            TouchDrag(); 
+        }
+
+        private void FixedUpdate()
+        {
+            
+            if (megaBend.angle == 0)
+                return;
+            
+            if (LeanTween.isPaused())
+                megaBend.angle += _megaBendAngleIncrease;
+
+            if (megaBend.angle > megaBendMaxAngle)
+                MoveWaxOutOfScreen();
+
+        }
+
+        private void MoveWaxOutOfScreen()
+        {
+            LeanTween.moveLocalY(gameObject, -10, 1f);
+        }
+
+        private void TouchDrag()
+        {
+
+            if (Input.touchCount <= 0)
+            {
+                
+                _megaBendAngleIncrease = -megaBendUpdateAngle;
+                
+                if (LeanTween.isPaused())
+                    LeanTween.resume(gameObject);
+                
+                return;
+                
+            }
+            
+            if (!LeanTween.isPaused())
+                LeanTween.pause(gameObject);
+            
+            var touch = Input.GetTouch(0);
+            if (touch.phase != TouchPhase.Stationary && touch.phase != TouchPhase.Moved) return;
+
+            _megaBendAngleIncrease = megaBendUpdateAngle;
+
+        }
+
+        private void DryWaxMesh()
+        {
+            LeanTween.alpha(meshRenderer.gameObject, 1.0f, 2f).setOnComplete(OnWaxMeshDry);
+        }
+
+        private void OnWaxMeshDry()
+        {
+            
+            hairs.SetActive(false);
+            LeanTween.value(gameObject, UpdateBendAngleValue, 0f, 15f, 3f).setLoopPingPong();
+            
+            GameManager.instance.SetState(new WaxRemoveState(GameManager.instance));
+            
+        }
+        
+        void UpdateBendAngleValue( float val )
+        {
+            megaBend.angle = val;
+        }
+
+    }
 }
--- Assets/Scripts/Controllers/WaxStickController.cs
+++ Assets/Scripts/Controllers/WaxStickController.cs
@@ -1,82 +1,90 @@
 using System;
-using GameState;
 using Obi;
 using UnityEngine;
 
-public class WaxStickController : MonoBehaviour
+namespace Controllers
 {
+    public class WaxStickController : MonoBehaviour
+    {
 
-    [SerializeField] private Camera camera;
-    [SerializeField] private ObiEmitter _obiEmitter;
-    [SerializeField] private int _emitSpeed = 2;
-    [SerializeField] private ObiParticleRenderer _obiParticleRenderer;
+        [SerializeField] private Camera camera;
+        [SerializeField] private ObiEmitter obiEmitter;
+        [SerializeField] private int emitSpeed = 2;
+        [SerializeField] private ObiParticleRenderer obiParticleRenderer;
 
-    public delegate void WaxStickAnimation();
-    public static event WaxStickAnimation OnWaxStickAnimationStopped;
+        public delegate void WaxStickAnimation();
+        public static event WaxStickAnimation OnWaxStickAnimationStopped;
     
-    private float   _cameraZDistance;
-    private Vector3 _screenPosition;
-    private Vector3 _newWorldPosition;
+        private float   _cameraZDistance;
+        private Vector3 _screenPosition;
+        private Vector3 _newWorldPosition;
     
-    private void Awake()
-    {
-        camera = camera ? camera : Camera.main;
-    }
-
-    private void Start()
-    {
-        _cameraZDistance  = camera.WorldToScreenPoint(transform.position).z;
-        _obiEmitter.speed = 0;
-    }
-
-    private void Update()
-    {
-         
-    }
-
-    private void OnMouseDrag()
-    {
-
-        float mousePosX = Input.mousePosition.x;
-        float mousePosY = Input.mousePosition.y;
-        
-        _screenPosition   = new Vector3(mousePosX, mousePosY, _cameraZDistance);
-        _newWorldPosition = camera.ScreenToWorldPoint(_screenPosition);
-
-        transform.position = _newWorldPosition;
-
-    }
-    
-    private void OnTriggerStay(Collider other)
-    {
-
-        if (ShouldAnimationStop())
+        private void Awake()
         {
-            StopAnimation();
+            camera = camera ? camera : Camera.main;
         }
 
-        _obiEmitter.speed = 2;
+        private void Start()
+        {
+            _cameraZDistance  = camera.WorldToScreenPoint(transform.position).z;
+            obiEmitter.speed = 0;
+        }
+
+        private void Update()
+        {
+            ControlWaxStick();
+        }
+
+        private void ControlWaxStick()
+        {
+
+            if (Input.touchCount <= 0) return;
+
+            var touch = Input.GetTouch(0);
+            if (touch.phase != TouchPhase.Stationary && touch.phase != TouchPhase.Moved) return;
+
+            var mousePosX = touch.position.x;
+            var mousePosY = touch.position.y;
+
+            _screenPosition = new Vector3(mousePosX, mousePosY, _cameraZDistance);
+            _newWorldPosition = camera.ScreenToWorldPoint(_screenPosition);
+
+            transform.position = _newWorldPosition;
+
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+
+            if (ShouldAnimationStop())
+            {
+                StopAnimation();
+            }
+
+            obiEmitter.speed = 2;
+
+        }
+
+        private bool ShouldAnimationStop()
+        {
+            return obiEmitter.activeParticleCount == obiEmitter.particleCount;
+        }
+
+        private void StopAnimation()
+        {
+            OnWaxStickAnimationStopped?.Invoke();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            obiEmitter.speed = 0;
+        }
+
+        public void DisableWaxStick()
+        {
+            obiParticleRenderer.enabled = false;
+        }
 
     }
-
-    private bool ShouldAnimationStop()
-    {
-        return _obiEmitter.activeParticleCount == _obiEmitter.particleCount;
-    }
-
-    private void StopAnimation()
-    {
-        OnWaxStickAnimationStopped?.Invoke();
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        _obiEmitter.speed = 0;
-    }
-
-    public void DisableWaxStick()
-    {
-        _obiParticleRenderer.enabled = false;
-    }
-
+    
 }
